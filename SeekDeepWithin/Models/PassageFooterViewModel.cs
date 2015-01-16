c268bbcc0bfb4bb695452a4dc609ddda7ced9b88
Jonathan Montiverdi <jpmon1@hotmail.com>
2015-01-16 06:28:22 -0600

    Adding missed files

---
+++ SeekDeepWithin/Models/PassageFooterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SeekDeepWithin.Domain;
+
+namespace SeekDeepWithin.Models
+{
+   /// <summary>
+   /// View model for a passage footer.
+   /// </summary>
+   public class PassageFooterViewModel : HeaderFooterViewModel
+   {
+      /// <summary>
+      /// Initializes a new header view model.
+      /// </summary>
+      /// <param name="footer">Footer to copy data from.</param>
+      public PassageFooterViewModel (PassageFooter footer) : base (footer) { }
+   }
+}
---
+++ SeekDeepWithin/Models/PassageHeaderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SeekDeepWithin.Domain;
+
+namespace SeekDeepWithin.Models
+{
+   /// <summary>
+   /// View model for a passage header.
+   /// </summary>
+   public class PassageHeaderViewModel : HeaderFooterViewModel
+   {
+      /// <summary>
+      /// Initializes a new header view model.
+      /// </summary>
+      /// <param name="header">Header to copy data from.</param>
+      public PassageHeaderViewModel (PassageHeader header) : base (header) { }
+   }
+}
